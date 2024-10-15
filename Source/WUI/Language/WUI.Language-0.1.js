d7991b11fafbc01181c80ae4d5bcf2de582dbe2f
Sergio Belmar <sbelmar@gmail.com>
2024-10-15 00:06:48 -0300

    Update WUI.Language-0.1.js

--- Source/WUI/Language/WUI.Language-0.1.js
+++ Source/WUI/Language/WUI.Language-0.1.js
@@ -89,7 +89,7 @@ class WUILanguage {
 						element.dataset[this._dataOutput] = text;
 					} else if (tagName.match(/^(meta)$/i)) {
 						element.setAttribute("content", text);
-					} else if (tagName.match(/^(div|span|h1|h2|h3|h4|h5|h6|p|li|a|legend|label|option|button)$/i)) {
+					} else if (tagName.match(/^(div|span|h1|h2|h3|h4|h5|h6|p|i|li|a|legend|label|option|button)$/i)) {
 						element.innerHTML = text;
 					} else if (tagName.match(/^(input|textarea)$/i)) {
 						element.setAttribute("placeholder", text);
